98564cd8a9ca218227ec64d29770d2368286dc81
Ubuntu <ubuntu@ip-172-31-24-44.us-east-2.compute.internal>
2021-11-09 15:27:44 +0000

    transferability

---
+++ transfer_attacks/Personalized_NN.py
@@ -0,0 +1,220 @@
+import torch
+import torch.nn as nn
+
+from collections import OrderedDict 
+import itertools
+
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+import random
+import csv
+import os
+import pickle
+from torch.autograd import Variable
+import copy
+
+from transfer_attacks.utils import *
+
+class Personalized_NN(nn.Module):
+    """
+    Pytorch NN module that combines head and neck weights for a layered based sharing system
+    For training federated learning neural network.
+    """
+    
+    def __init__(self, trained_network):
+        
+        # Init attributes
+        super(Personalized_NN, self).__init__()
+        self.trained_network = trained_network
+        self.criterion = nn.NLLLoss()
+        self.cuda = True
+        
+        # test_acc attributes
+        self.orig_test_acc = None
+        self.adv_test_acc = None
+        
+        # Split Test att
+        self.orig_test_acc_robust = None
+        self.orig_output_sim_robust = None
+        self.orig_test_acc_adv = None
+        self.orig_output_sim_adv = None
+        
+        self.orig_output_sim = None
+        self.adv_output_sim = None
+        
+        self.orig_target_achieve = None
+        self.adv_target_achieve = None
+        
+        # Log correct vs. Incorrect Indices
+        self.adv_indices = None # tensor List of indices from batch that are fooled
+        self.robust_indices = None # List of indices from batch robust
+        
+    def forward(self,x):
+        
+        if torch.cuda.is_available():
+            x = x.cuda()
+        
+        x = self.trained_network.forward(x)
+        
+        return x
+    
+    def forward_transfer(self, x_orig, x_adv, y_orig, y_adv,
+                         true_labels, target, print_info = False):
+        """
+        Assume that input images are in pytorch tensor format
+        """
+        self.eval()
+        
+        # Cuda Availability
+        if torch.cuda.is_available():
+            (y_orig, y_adv) = (y_orig.cuda(), y_adv.cuda())
+        
+        batch_size = y_orig.shape[0]
+        
+        # Forward Two Input Types
+        h_adv = self.forward(x_adv)
+        h_orig = self.forward(x_orig)
+        h_adv_category = torch.argmax(h_adv,dim = 1)
+        h_orig_category = torch.argmax(h_orig,dim = 1)
+        
+        # Record Different Parameters
+        self.orig_test_acc = (h_orig_category == true_labels).float().sum()/batch_size
+        self.adv_test_acc = (h_adv_category == true_labels).float().sum()/batch_size
+        
+        self.orig_output_sim = (h_orig_category == y_orig).float().sum()/batch_size
+        self.adv_output_sim = (h_adv_category == y_adv).float().sum()/batch_size
+        
+        self.orig_target_achieve = (h_orig_category == target).float().sum()/batch_size
+        self.adv_target_achieve = (h_adv_category == target).float().sum()/batch_size
+        
+        # Record based on indices
+        self.adv_indices = h_adv_category == target
+        self.robust_indices = h_adv_category != target
+        
+        # Record split losses
+        self.orig_test_acc_robust = (h_orig_category[self.robust_indices] == true_labels[self.robust_indices]).float().sum()/h_orig_category[self.robust_indices].shape[0]
+        self.orig_output_sim_robust = (h_orig_category[self.robust_indices] == y_orig[self.robust_indices]).float().sum()/h_orig_category[self.robust_indices].shape[0]
+        
+        self.orig_test_acc_adv = (h_orig_category[self.adv_indices] == true_labels[self.adv_indices]).float().sum()/h_orig_category[self.adv_indices].shape[0]
+        self.orig_output_sim_adv = (h_orig_category[self.adv_indices] == y_orig[self.adv_indices]).float().sum()/h_orig_category[self.adv_indices].shape[0]
+            
+
+        # Print Relevant Information
+        if print_info:
+            print("---- Attack Transfer:", "----\n")
+            print("         Orig Test Acc:", self.orig_test_acc.item())
+            print("          Adv Test Acc:", self.adv_test_acc.item())
+            print("Orig Output Similarity:", self.orig_output_sim.item())
+            print(" Adv Output Similarity:", self.adv_output_sim.item())
+            print("       Orig Target Hit:", self.orig_target_achieve.item())
+            print("        Adv Target Hit:", self.adv_target_achieve.item())
+            
+        return (h_adv, h_orig, h_adv_category, h_orig_category)
+
+
+class Adv_NN(Personalized_NN):
+    
+    def __init__(self, trained_network, dataloader):
+        
+        # Init attributes
+        super(Adv_NN, self).__init__(trained_network)
+        
+        self.dataloader = dataloader
+        
+        # Attack outputs
+        self.x_orig = None
+        self.x_adv = None
+        self.y_orig = None
+        
+        self.softmax_orig = None
+        self.output_orig = None
+        self.softmax_adv = None
+        self.output_adv = None 
+        
+        
+    def i_fgsm(self, atk_params, print_info=False, mode = 'test'):
+        """
+            Perform IFSGM attack on a randomly sampled batch 
+            All attack params and batch sizes are defiend in atk_params
+        """
+        self.eval()
+        
+        # Import attack parameters
+        batch_size = atk_params.batch_size
+        target= atk_params.target
+        eps= atk_params.eps
+        alpha= atk_params.alpha
+        iteration= atk_params.iteration
+        x_val_min= atk_params.x_val_min
+        x_val_max= atk_params.x_val_max
+        
+        # Load data to perturb
+    
+        data_x, data_y = self.dataloader.load_batch(batch_size, mode=mode)
+        self.x_orig  = data_x.reshape(batch_size,3,32,32)
+        self.y_orig = data_y.type(torch.LongTensor)
+        
+        if torch.cuda.is_available():
+            self.y_orig = self.y_orig.cuda()
+        
+        self.target = target
+        
+        self.x_adv = Variable(self.x_orig, requires_grad=True)
+        
+        for i in range(iteration):
+            
+            h_adv = self.forward(self.x_adv)
+            
+            # Loss function based on target
+            if target > -1:
+                target_tensor = torch.LongTensor(self.y_orig.size()).fill_(target)
+                target_tensor = Variable(cuda(target_tensor, self.cuda), requires_grad=False)
+                cost = self.criterion(h_adv, target_tensor)
+            else:
+                cost = -self.criterion(h_adv, self.y_orig)
+
+            self.zero_grad()
+
+            if self.x_adv.grad is not None:
+                self.x_adv.grad.data.fill_(0)
+            cost.backward()
+
+            self.x_adv.grad.sign_()
+            self.x_adv = self.x_adv - alpha*self.x_adv.grad
+            self.x_adv = where(self.x_adv > self.x_orig+eps, self.x_orig+eps, self.x_adv)
+            self.x_adv = where(self.x_adv < self.x_orig-eps, self.x_orig-eps, self.x_adv)
+            self.x_adv = torch.clamp(self.x_adv, x_val_min, x_val_max)
+            self.x_adv = Variable(self.x_adv.data, requires_grad=True)
+
+        self.post_attack(batch_size = batch_size, print_info = print_info)
+            
+    def post_attack(self, batch_size, print_info = False):
+        """
+        Computes attack success metrics after xadv is generated
+        """
+                
+        self.softmax_orig = self.forward(self.x_orig)
+        self.output_orig = torch.argmax(self.softmax_orig,dim=1)
+        self.softmax_adv = self.forward(self.x_adv)
+        self.output_adv = torch.argmax(self.softmax_adv,dim=1)
+        
+        # Record accuracy and loss
+        self.orig_loss = self.criterion(self.softmax_orig, self.y_orig).item()
+        self.adv_loss = self.criterion(self.softmax_adv, self.y_orig).item()
+        self.orig_acc = (self.output_orig == self.y_orig).float().sum()/batch_size
+        self.adv_acc = (self.output_adv == self.y_orig).float().sum()/batch_size
+        
+        # Add Perturbation Distance (L2 norm) - across each input
+        self.norm = torch.norm(torch.sub(self.x_orig, self.x_adv, alpha=1),dim=(2,3))
+
+        # Print Relevant Information
+        if print_info:
+            print("---- FGSM Batch Size:", batch_size, "----\n")
+            print("Orig Target:", self.y_orig.tolist())
+            print("Orig Output:", self.output_orig.tolist())
+            print("ADV Output :", self.output_adv.tolist(),'\n')
+            print("Orig Loss  :", self.orig_loss)
+            print("ADV Loss   :", self.adv_loss,'\n')
+            print("Orig Acc   :", self.orig_acc.item())
+            print("ADV Acc    :", self.adv_acc.item())
---
+++ transfer_attacks/utils.py
@@ -0,0 +1,175 @@
+import argparse, torch
+import numpy as np
+from torch import nn
+from torch.autograd import Variable
+from pathlib import Path
+
+# Import General Libraries
+import os
+import argparse
+import copy
+import pickle
+import random
+
+# Import FedEM based Libraries
+from utils.utils import *
+from utils.constants import *
+from utils.args import *
+from torch.utils.tensorboard import SummaryWriter
+from run_experiment import *
+from models import *
+
+
+class One_Hot(nn.Module):
+    # from :
+    # https://lirnli.wordpress.com/2017/09/03/one-hot-encoding-in-pytorch/
+    def __init__(self, depth):
+        super(One_Hot,self).__init__()
+        self.depth = depth
+        self.ones = torch.sparse.torch.eye(depth)
+    def forward(self, X_in):
+        X_in = X_in.long()
+        return Variable(self.ones.index_select(0,X_in.data))
+    def __repr__(self):
+        return self.__class__.__name__ + "({})".format(self.depth)
+
+
+def cuda(tensor,is_cuda):
+    if is_cuda : return tensor.cuda()
+    else : return tensor
+
+def str2bool(v):
+    # codes from : https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
+
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+def print_network(net):
+    num_params = 0
+    for param in net.parameters():
+        num_params += param.numel()
+    print(net)
+    print('Total number of parameters: %d' % num_params)
+
+
+def rm_dir(dir_path, silent=True):
+    p = Path(dir_path).resolve()
+    if (not p.is_file()) and (not p.is_dir()) :
+        print('It is not path for file nor directory :',p)
+        return
+
+    paths = list(p.iterdir())
+    if (len(paths) == 0) and p.is_dir() :
+        p.rmdir()
+        if not silent : print('removed empty dir :',p)
+
+    else :
+        for path in paths :
+            if path.is_file() :
+                path.unlink()
+                if not silent : print('removed file :',path)
+            else:
+                rm_dir(path)
+        p.rmdir()
+        if not silent : print('removed empty dir :',p)
+
+def where(cond, x, y):
+    """
+    code from :
+        https://discuss.pytorch.org/t/how-can-i-do-the-operation-the-same-as-np-where/1329/8
+    """
+    cond = cond.float()
+    return (cond*x) + ((1-cond)*y)
+
+def dummy_aggregator(args_):
+
+    torch.manual_seed(args_.seed)
+
+    data_dir = get_data_dir(args_.experiment)
+
+    if "logs_root" in args_:
+        logs_root = args_.logs_root
+    else:
+        logs_root = os.path.join("logs", args_to_string(args_))
+
+    print("==> Clients initialization..")
+    clients = init_clients(
+        args_,
+        root_path=os.path.join(data_dir, "train"),
+        logs_root=os.path.join(logs_root, "train")
+    )
+
+    print("==> Test Clients initialization..")
+    test_clients = init_clients(
+        args_,
+        root_path=os.path.join(data_dir, "test"),
+        logs_root=os.path.join(logs_root, "test")
+    )
+
+    logs_path = os.path.join(logs_root, "train", "global")
+    os.makedirs(logs_path, exist_ok=True)
+    global_train_logger = SummaryWriter(logs_path)
+
+    logs_path = os.path.join(logs_root, "test", "global")
+    os.makedirs(logs_path, exist_ok=True)
+    global_test_logger = SummaryWriter(logs_path)
+
+    global_learners_ensemble = \
+        get_learners_ensemble(
+            n_learners=args_.n_learners,
+            name=args_.experiment,
+            device=args_.device,
+            optimizer_name=args_.optimizer,
+            scheduler_name=args_.lr_scheduler,
+            initial_lr=args_.lr,
+            input_dim=args_.input_dimension,
+            output_dim=args_.output_dimension,
+            n_rounds=args_.n_rounds,
+            seed=args_.seed,
+            mu=args_.mu
+    )
+
+
+    if args_.decentralized:
+        aggregator_type = 'decentralized'
+    else:
+        aggregator_type = AGGREGATOR_TYPE[args_.method]
+
+    aggregator =\
+        get_aggregator(
+            aggregator_type=aggregator_type,
+            clients=clients,
+            global_learners_ensemble=global_learners_ensemble,
+            lr_lambda=args_.lr_lambda,
+            lr=args_.lr,
+            q=args_.q,
+            mu=args_.mu,
+            communication_probability=args_.communication_probability,
+            sampling_rate=args_.sampling_rate,
+            log_freq=args_.log_freq,
+            global_train_logger=global_train_logger,
+            global_test_logger=global_test_logger,
+            test_clients=test_clients,
+            verbose=args_.verbose,
+            seed=args_.seed
+        )
+
+    return aggregator, clients
+
+# Write a Custom Class for Dataloader that has flexible batch size
+class Custom_Dataloader:
+    def __init__(self, x_data, y_data):
+        self.x_data = x_data # Tensor + cuda
+        self.y_data = y_data # Tensor + cuda
+        
+    def load_batch(self,batch_size,mode = 'test'):
+        samples = random.sample(range(self.y_data.shape[0]),batch_size)
+        out_x_data = self.x_data[samples].to(device='cuda')
+        out_y_data = self.y_data[samples].to(device='cuda')
+        
+        return out_x_data, out_y_data
