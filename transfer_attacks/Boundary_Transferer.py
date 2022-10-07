a931144a21e4a6ba03390b57f967fa6a6de9747d
tj-kim <taejin.kim96@gmail.com>
2022-10-07 22:31:37 +0000

    variance bars add

--- transfer_attacks/Boundary_Transferer.py
+++ transfer_attacks/Boundary_Transferer.py
@@ -250,8 +250,8 @@ class Boundary_Transferer():
             self.atk_params.target = self.y_comparison
         
         # 1.1.22 Changed from ifsgm sub to pgd sub)
-        # self.advNN.pgd_sub(self.atk_params,x_in.unsqueeze(0),y_in.unsqueeze(0))
-        self.advNN.i_fgsm_sub(self.atk_params,x_in.unsqueeze(0),y_in.unsqueeze(0))
+        self.advNN.pgd_sub(self.atk_params,x_in.unsqueeze(0),y_in.unsqueeze(0))
+#         self.advNN.i_fgsm_sub(self.atk_params,x_in.unsqueeze(0),y_in.unsqueeze(0))
         
         x_adv = self.advNN.x_adv
         dist_diff = torch.subtract(x_adv, x_in)
